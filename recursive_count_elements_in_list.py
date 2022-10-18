27c0a4d5df7b1498ca82142f98ccc004e2efa5e6
Vladimir Danilov <voffka.danilov@gmail.com>
2022-10-18 16:03:59 +0300

    Added recursive algorithm for counting elements in the list

---
+++ recursive_count_elements_in_list.py
@@ -0,0 +1,11 @@
+# Grokking algorithms, p. 85, exercise 4.2
+# Рекурсивная функция подсчета элементов в списке
+
+def count_elements(my_list):
+    if not my_list:
+        return 0
+    return 1 + count_elements(my_list[1:])
+
+
+# test zone
+print(count_elements([1, 2, 3, 4, 5, 6]))
